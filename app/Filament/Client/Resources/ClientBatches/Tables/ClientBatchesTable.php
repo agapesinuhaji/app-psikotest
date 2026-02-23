c0910cd84021ef81d748a166bdfb664310a6b161
unknown <agapemanase27@gmail.com>
2026-02-23 13:44:43 +0700

    Revisi sedikit urutan data batch

--- app/Filament/Client/Resources/ClientBatches/Tables/ClientBatchesTable.php
+++ app/Filament/Client/Resources/ClientBatches/Tables/ClientBatchesTable.php
@@ -14,6 +14,7 @@ class ClientBatchesTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
 
                 TextColumn::make('name')
